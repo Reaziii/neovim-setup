c605720d9e12336c1b990fe15ae196fdf19911c7
Reaz Ahammed <baphonreaz@gmail.com>
2024-12-19 23:02:32 +0600

    add typescript support

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,28 @@
 return {
-  "nvim-telescope/telescope-ui-select.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim"
-  },
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional, for file icons
   config = function()
-    require("telescope").setup {
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown {
-          }
-        }
-      }
-    }
-    require("telescope").load_extension("ui-select")
-  end
+    local fzf = require("fzf-lua")
+    fzf.setup({
+      files = {
+        -- Use a POSIX-compatible find command
+        cmd = "find . -type f -not -path '*/\\.git/*'",
+        prompt = "Files❯ ",
+        git_icons = true, -- Show git icons
+        file_icons = true, -- Show file icons
+        color_icons = true, -- Enable color for icons
+      },
+      grep = {
+        -- Use rg if available, fallback to grep
+        cmd = "rg --vimgrep",
+        prompt = "Rg❯ ",
+      },
+    })
+
+    -- Keybindings for quick access
+    vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", fzf.grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Buffers" })
+    vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help Tags" })
+  end,
 }
